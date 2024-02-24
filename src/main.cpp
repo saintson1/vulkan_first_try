d7a5dab77d46170946fc9061cafb6c812e2a2fe4
saintson <pan.alexander2006@gmail.com>
2024-02-25 00:02:15 +0300

    second branch check

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include "app/application.hpp"
 #include "render/resource/resourse_def.hpp"
+#include "math/math_def.hpp"
 
 int main( int argc, char *argv[] )
 {
-  const std::string proj_path = "E:/visual_studio/First-Vulkan-Try/";
   vulkan_ft::application::GetInstancePtr("pupupu", 100, 100);
-
-  vulkan_ft::shader( proj_path + "resource/shader/", "first_triangle");
 
   return 0;
 } // End of MAIN function
